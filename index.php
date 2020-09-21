4c5f7debf2127cf86c95aff048b1280dc55883d3
Hugo Araujo <araujohugo@msn.com>
2020-09-21 14:00:16 +0200

    Debugage
    Debugage de la page d'acceuil

--- index.php
+++ index.php
@@ -1,39 +1,53 @@
-<? 
+<!DOCTYPE html>
+<html lang="fr">
 
-include "_debut.inc.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MissionFestival</title>
+    <?php 
+   include "_debut.inc.php";
+   ?>
+</head>
 
-echo " 
-<br> 
-<table width='80%' cellspacing='0' cellpadding='0' align='center'>
-   <tr>  
-      <td class='texteAccueil'>
-         Cette application web permet de gérer l'hébergement des groupes de musique 
-         durant le festival Folklores du Monde.
-      </td>
-   </tr>
-   <tr>
-      <td>&nbsp;
-      </td>
-   </tr>
-   <tr>
-      <td class='texteAccueil'>
-          Elle offre les services suivants :
-      </td>
-   </tr>
-   <tr>
-      <td>&nbsp;
-      </td>
-   </tr>
-   <tr>
-      <td class='texteAccueil'>
-      <ul>
-         <li>Gérer les établissements (caractéristiques et capacités d'accueil) acceptant d'héberger les groupes de musiciens.
-         <p>
-	      </p>
-         <li>Consulter, réaliser ou modifier les attributions des chambres aux groupes dans les établissements.
-      </ul>
-      </td>
-   </tr>
-</table>";
+<body>
 
-?>
+    <br>
+    <table width='80%' cellspacing='0' cellpadding='0' align='center'>
+        <tr>
+            <td class='texteAccueil'>
+                Cette application web permet de gérer l'hébergement des groupes de musique
+                durant le festival Folklores du Monde.
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td class='texteAccueil'>
+                Elle offre les services suivants :
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td class='texteAccueil'>
+                <ul>
+                    <li>Gérer les établissements (caractéristiques et capacités d'accueil) acceptant d'héberger les
+                        groupes
+                        de musiciens.
+                        <p>
+                        </p>
+                    <li>Consulter, réaliser ou modifier les attributions des chambres aux groupes dans les
+                        établissements.
+                </ul>
+            </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
