34b293c15b8bca943a06784831c9994d950a46e8
Hugo Araujo <araujohugo@msn.com>
2020-09-21 15:38:16 +0200

    Ajout BDD
    Ajout de la BDD modifier

--- SQL/create-utf8.sql
+++ SQL/create-utf8.sql
@@ -1,42 +1,51 @@
 
-GRANT ALL ON festival . * TO 'festival'@'localhost' IDENTIFIED BY 'secret';
+USE FestivalM2L;
 
 DROP TABLE IF EXISTS `Attribution`;
-DROP TABLE IF EXISTS `Groupe`;
+DROP TABLE IF EXISTS `Equipe`;
 DROP TABLE IF EXISTS `Etablissement`;
-
+DROP TABLE IF EXISTS `Sport`;
+ 
 create table Etablissement 
-(id char(8) not null, 
+(idEtablissement char(8) not null, 
 nom varchar(45) not null,
 adresseRue varchar(45) not null, 
 codePostal char(5) not null, 
 ville varchar(35) not null,
 tel varchar(13) not null,
 adresseElectronique varchar(70),
-type tinyint not null,
+type integer(1) not null,
 civiliteResponsable varchar(12) not null,
 nomResponsable varchar(25) not null,
 prenomResponsable varchar(25),
 nombreChambresOffertes integer,
-constraint pk_Etablissement primary key(id))
+constraint pk_Etablissement primary key(idEtablissement))
 ENGINE=INNODB;
 
-create table Groupe
-(id char(4) not null, 
+create table Sport
+(idSport integer(2) not null,
+libelleSport varchar(45) not null,
+constraint pk_Sport primary key(idSport))
+ENGINE=INNODB;
+
+create table Equipe
+(idE char(4) not null, 
 nom varchar(40) not null, 
 identiteResponsable varchar(40) default null,
 adressePostale varchar(120) default null,
 nombrePersonnes integer not null, 
 nomPays varchar(40) not null, 
-hebergement char(1) not null,
-constraint pk_Groupe primary key(id))
+hebergement char(1) not null, 
+numSport integer(2) not null,
+constraint pk_Equipe primary key(idE),
+constraint fk_Equipe foreign key(numSport) references Sport(idSport))
 ENGINE=INNODB;
 
 create table Attribution
 (idEtab char(8) not null, 
-idGroupe char(4) not null, 
+idEquipe char(4) not null, 
 nombreChambres integer not null,
-constraint pk_Attribution primary key(idEtab, idGroupe), 
-constraint fk1_Attribution foreign key(idEtab) references Etablissement(id),
-constraint fk2_Attribution foreign key(idGroupe) references Groupe(id)) 
+constraint pk_Attribution primary key(idEtab, idEquipe), 
+constraint fk1_Attribution foreign key(idEtab) references Etablissement(idEtablissement),
+constraint fk2_Attribution foreign key(idEquipe) references Equipe(idE)) 
 ENGINE=INNODB;
--- SQL/insert-utf8.sql
+++ SQL/insert-utf8.sql
@@ -1,55 +1,56 @@
 
 -- Certains établissements sont fictifs
-insert into Etablissement values ('0350785N', 'Collège de Moka', '2 avenue Aristide Briand BP 6', '35401', 'Saint-Malo', '0299206990', null,1,'M.','Dupont','Alain',20);
-insert into Etablissement values ('0350123A', 'Collège Lamartine', '3, avenue des corsaires', '35404', 'Paramé', '0299561459', null, 1,'Mme','Lefort','Anne',58);  
+insert into Etablissement values ('0350785N', 'Collège de Moka', '2 avenue Aristide Briand BP 6', '59000', 'Nancy', '0299206990', null,1,'M.','Dupont','Alain',20);
+insert into Etablissement values ('0350123A', 'Collège Louis Vignac', '12, avenue Douwi', '35404', 'Paramé', '0299561459', null, 1,'Mme','Lefort','Anne',58);  
 insert into Etablissement values ('0351234W', 'Collège Léonard de Vinci', '2 rue Rabelais', '35418', 'Saint-Malo', '0299117474', null, 1,'M.','Durand','Pierre',60);   
 insert into Etablissement values ('11111111', 'Centre de rencontres internationales', '37 avenue du R.P. Umbricht BP 108', '35407', 'Saint-Malo', '0299000000', null, 0, 'M.','Guenroc','Guy',200);
 
--- Certains groupes sont incomplètement renseignés
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g001','Groupe folklorique du Bachkortostan',40,'Bachkirie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g002','Marina Prudencio Chavez',25,'Bolivie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g003','Nangola Bahia de Salvador',34,'Brésil','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g004','Bizone de Kawarma',38,'Bulgarie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g005','Groupe folklorique camerounais',22,'Cameroun','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g006','Syoung Yaru Mask Dance Group',29,'Corée du Sud','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g007','Pipe Band',19,'Ecosse','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g008','Aira da Pedra',5,'Espagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g009','The Jersey Caledonian Pipe Band',21,'Jersey','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g010','Groupe folklorique des Émirats',30,'Emirats arabes unis','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g011','Groupe folklorique mexicain',38,'Mexique','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g012','Groupe folklorique de Panama',22,'Panama','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g013','Groupe folklorique papou',13,'Papouasie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g014','Paraguay Ete',26,'Paraguay','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g015','La Tuque Bleue',8,'Québec','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g016','Ensemble Leissen de Oufa',40,'République de Bachkirie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g017','Groupe folklorique turc',40,'Turquie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g018','Groupe folklorique russe',43,'Russie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g019','Ruhunu Ballet du village de Kosgoda',27,'Sri Lanka','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g020','L''Alen',34,'France - Provence','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g021','L''escolo Di Tourre',40,'France - Provence','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g022','Deloubes Kévin',1,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g023','Daonie See',5,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g024','Boxty',5,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g025','Soeurs Chauvel',2,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g026','Cercle Gwik Alet',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g027','Bagad Quic En Groigne',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g028','Penn Treuz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g029','Savidan Launay',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g030','Cercle Boked Er Lann',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g031','Bagad Montfortais',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g032','Vent de Noroise',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g033','Cercle Strollad',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g034','Bagad An Hanternoz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g035','Cercle Ar Vro Melenig',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g036','Cercle An Abadenn Nevez',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g037','Kerc''h Keltiek Roazhon',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g038','Bagad Plougastel',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g039','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g040','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g041','Jackie Molard Quartet',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g042','Deomp',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g043','Cercle Olivier de Clisson',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g044','Kan Tri',0,'France - Bretagne','N');
+
+-- Certains Equipes sont incomplètement renseignés
+insert into Equipe(id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g001','Nancy football club',40,'Bachkirie','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g002','Equipe Marina foot',25,'Bolivie','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g003','NBSalvador Basketball',34,'Brésil','O',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g004','Bizone Handball',38,'Bulgarie','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g005','Equipe foot St-Louis',22,'Cameroun','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g006','Syoung Yaru Mask Athletisme Group',29,'Corée du Sud','O',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g007','Pipe Basketball Association',19,'Ecosse','O',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g008','SLP Foot',5,'Espagne','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g009','The Jersey Caledonian Handball Association',21,'Jersey','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g010','Caramoan FCA',30,'Emirats arabes unis','O',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g011','Association de Football Fidjienne',38,'Mexique','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g012','Panama Hand Club',22,'Panama','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g013','Papou FC',13,'Papouasie','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g014','Olympique Paraguay Athletisme (OPA) ',26,'Paraguay','O',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g015','La Tuque Handball Association',8,'Québec','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g016','Ensemble Leissen de Football',40,'République de Bachkirie','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g017','Equipe footbalistique  turc',40,'Turquie','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g018','Equipe Handball russe',43,'Russie','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g019','Association de Football des villages du Kosgoda (AFVK)',27,'Sri Lanka','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g020','Van Haleine FC',34,'France - Provence','O',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g021','Equipe de Basketball Italien',40,'France - Provence','O',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g022','Kevin fait du Handball',1,'France - Bretagne','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g023','Daonie Athletisme Association',5,'France - Bretagne','O',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g024','Olympique Boxty Handball ',5,'France - Bretagne','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g025','Club Chauvin de Handball ',2,'France - Bretagne','O',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g026','Gwik Alet Basketball Association',0,'France - Bretagne','N',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g027','Foot en bretagne ',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g028','Charenton-La Tongue Club Athlé ',0,'France - Bretagne','N',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g029','Issou Football Club',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g030','Varcelone Football Club',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g031','Uefa-Madrid FC',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g032','Handball Nordique',0,'France - Bretagne','N',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g033','Athletisme Deter du sud est ',0,'France - Bretagne','N',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g034','ACC Basketball',0,'France - Bretagne','N',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g035','Cercle Athlé espagnol',0,'France - Bretagne','N',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g036','Paraprout Football ',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g037','Handball Club du Quintanach Breton',0,'France - Bretagne','N',3);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g038','Plougastel Athlé',0,'France - Bretagne','N',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g039','BCH FC',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g040','Blody-Hell Baketball',0,'France - Bretagne','N',2);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g041','Molard FC',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g042','Cercle Athlétisme Nord',0,'France - Bretagne','N',4);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g043','Football south island',0,'France - Bretagne','N',1);
+insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g044','Equipe Foot AntoineL',0,'France - Bretagne','N',1);
 
 -- Les attributions sont fictives
 insert into Attribution values ('0350785N', 'g001', 11);
@@ -61,6 +62,12 @@ insert into Attribution values ('0350123A', 'g005', 8);
 insert into Attribution values ('0351234W', 'g001', 3);
 insert into Attribution values ('0351234W', 'g006', 10);
 insert into Attribution values ('0351234W', 'g007', 7);
+
+insert into Sport (idSport, libelleSport) values (1,'Football');
+insert into Sport (idSport, libelleSport) values (2,'Basketball');
+insert into Sport (idSport, libelleSport) values (3,'Handball');
+insert into Sport (idSport, libelleSport) values (4,'Athletisme');
+
 
 
 
