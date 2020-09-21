77fe5595b9a10eafb3fef72c82c897ceaf8d1ae8
Hugo Araujo <araujohugo@msn.com>
2020-09-21 14:59:29 +0200

    Amélioration Acceuil
    Amélioration de l'interface de l'acceuil et de la barre

--- _debut.inc.php
+++ _debut.inc.php
@@ -1,35 +1,38 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
-"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <!-- TITRE ET MENUS -->
 <html lang="fr">
+
 <head>
-<title>Festival</title>
-<meta http-equiv="Content-Language" content="fr">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="css/cssGeneral.css" rel="stylesheet" type="text/css">
+    <title>Festival</title>
+    <meta http-equiv="Content-Language" content="fr">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/rose.css">
 </head>
-<body class="basePage">
 
-<!--  Tableau contenant le titre -->
-<table width="100%" cellpadding="0" cellspacing="0">
-   <tr> 
-      <td class="titre">Festival Folklores du Monde <br>
-      <span id="texteNiveau2" class="texteNiveau2">
-      H&eacute;bergement des groupes</span><br>&nbsp;
-      </td>
-   </tr>
+<center> <img src="images\logo-200x200.png"> </center>
+
+<ul class="nav justify-content-center nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link" href="index.php">Acceuil</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="listeEtablissements.php">Gestion établissements</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="consultationAttributions.php">Attributions chambres</a>
+    </li>
+</ul>
+
 </table>
-
-<!--  Tableau contenant les menus -->
-<table width="80%" cellpadding="0" cellspacing="0" class="tabMenu" align="center">
-   <tr>
-      <td class="menu"><a href="index.php">Accueil</a></td>
-      <td class="menu"><a href="listeEtablissements.php">
-      Gestion établissements</a></td>
-      <td class="menu"><a href="consultationAttributions.php">
-      Attributions chambres</a></td>
-   </tr>
-</table>
-<br>';
-
-
+<br>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+</script>
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php 
+   include "_debut.inc.php";
+   ?>
