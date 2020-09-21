c8b44618163f8d789e3d4e3bcf0925d1dfb5436a
RomainTH2002 <67055595+RomainTH2002@users.noreply.github.com>
2020-09-21 16:06:51 +0200

    Update insert-utf8.sql

--- SQL/insert-utf8.sql
+++ SQL/insert-utf8.sql
@@ -5,52 +5,57 @@ insert into Etablissement values ('0350123A', 'Collège Louis Vignac', '12, aven
 insert into Etablissement values ('0351234W', 'Collège Léonard de Vinci', '2 rue Rabelais', '35418', 'Saint-Malo', '0299117474', null, 1,'M.','Durand','Pierre',60);   
 insert into Etablissement values ('11111111', 'Centre de rencontres internationales', '37 avenue du R.P. Umbricht BP 108', '35407', 'Saint-Malo', '0299000000', null, 0, 'M.','Guenroc','Guy',200);
 
+insert into Sport (idSport, libelleSport) values (1,'Football');
+insert into Sport (idSport, libelleSport) values (2,'Basketball');
+insert into Sport (idSport, libelleSport) values (3,'Handball');
+insert into Sport (idSport, libelleSport) values (4,'Athletisme');
+
 
 -- Certains Equipes sont incomplètement renseignés
-insert into Equipe(id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g001','Nancy football club',40,'Bachkirie','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g002','Equipe Marina foot',25,'Bolivie','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g003','NBSalvador Basketball',34,'Brésil','O',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g004','Bizone Handball',38,'Bulgarie','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g005','Equipe foot St-Louis',22,'Cameroun','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g006','Syoung Yaru Mask Athletisme Group',29,'Corée du Sud','O',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g007','Pipe Basketball Association',19,'Ecosse','O',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g008','SLP Foot',5,'Espagne','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g009','The Jersey Caledonian Handball Association',21,'Jersey','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g010','Caramoan FCA',30,'Emirats arabes unis','O',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g011','Association de Football Fidjienne',38,'Mexique','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g012','Panama Hand Club',22,'Panama','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g013','Papou FC',13,'Papouasie','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g014','Olympique Paraguay Athletisme (OPA) ',26,'Paraguay','O',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g015','La Tuque Handball Association',8,'Québec','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g016','Ensemble Leissen de Football',40,'République de Bachkirie','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g017','Equipe footbalistique  turc',40,'Turquie','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g018','Equipe Handball russe',43,'Russie','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g019','Association de Football des villages du Kosgoda (AFVK)',27,'Sri Lanka','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g020','Van Haleine FC',34,'France - Provence','O',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g021','Equipe de Basketball Italien',40,'France - Provence','O',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g022','Kevin fait du Handball',1,'France - Bretagne','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g023','Daonie Athletisme Association',5,'France - Bretagne','O',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g024','Olympique Boxty Handball ',5,'France - Bretagne','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g025','Club Chauvin de Handball ',2,'France - Bretagne','O',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g026','Gwik Alet Basketball Association',0,'France - Bretagne','N',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g027','Foot en bretagne ',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g028','Charenton-La Tongue Club Athlé ',0,'France - Bretagne','N',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g029','Issou Football Club',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g030','Varcelone Football Club',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g031','Uefa-Madrid FC',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g032','Handball Nordique',0,'France - Bretagne','N',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g033','Athletisme Deter du sud est ',0,'France - Bretagne','N',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g034','ACC Basketball',0,'France - Bretagne','N',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g035','Cercle Athlé espagnol',0,'France - Bretagne','N',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g036','Paraprout Football ',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g037','Handball Club du Quintanach Breton',0,'France - Bretagne','N',3);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g038','Plougastel Athlé',0,'France - Bretagne','N',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g039','BCH FC',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g040','Blody-Hell Baketball',0,'France - Bretagne','N',2);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g041','Molard FC',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g042','Cercle Athlétisme Nord',0,'France - Bretagne','N',4);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g043','Football south island',0,'France - Bretagne','N',1);
-insert into Equipe (id, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g044','Equipe Foot AntoineL',0,'France - Bretagne','N',1);
+insert into Equipe(idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g001','Nancy football club',40,'Bachkirie','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g002','Equipe Marina foot',25,'Bolivie','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g003','NBSalvador Basketball',34,'Brésil','O',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g004','Bizone Handball',38,'Bulgarie','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g005','Equipe foot St-Louis',22,'Cameroun','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g006','Syoung Yaru Mask Athletisme Group',29,'Corée du Sud','O',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g007','Pipe Basketball Association',19,'Ecosse','O',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g008','SLP Foot',5,'Espagne','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g009','The Jersey Caledonian Handball Association',21,'Jersey','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g010','Caramoan FCA',30,'Emirats arabes unis','O',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g011','Association de Football Fidjienne',38,'Mexique','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g012','Panama Hand Club',22,'Panama','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g013','Papou FC',13,'Papouasie','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g014','Olympique Paraguay Athletisme (OPA) ',26,'Paraguay','O',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g015','La Tuque Handball Association',8,'Québec','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g016','Ensemble Leissen de Football',40,'République de Bachkirie','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g017','Equipe footbalistique  turc',40,'Turquie','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g018','Equipe Handball russe',43,'Russie','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g019','Association de Football des villages du Kosgoda (AFVK)',27,'Sri Lanka','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g020','Van Haleine FC',34,'France - Provence','O',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g021','Equipe de Basketball Italien',40,'France - Provence','O',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g022','Kevin fait du Handball',1,'France - Bretagne','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g023','Daonie Athletisme Association',5,'France - Bretagne','O',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g024','Olympique Boxty Handball ',5,'France - Bretagne','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g025','Club Chauvin de Handball ',2,'France - Bretagne','O',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g026','Gwik Alet Basketball Association',0,'France - Bretagne','N',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g027','Foot en bretagne ',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g028','Charenton-La Tongue Club Athlé ',0,'France - Bretagne','N',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g029','Issou Football Club',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g030','Varcelone Football Club',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g031','Uefa-Madrid FC',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g032','Handball Nordique',0,'France - Bretagne','N',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g033','Athletisme Deter du sud est ',0,'France - Bretagne','N',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g034','ACC Basketball',0,'France - Bretagne','N',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g035','Cercle Athlé espagnol',0,'France - Bretagne','N',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g036','Paraprout Football ',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g037','Handball Club du Quintanach Breton',0,'France - Bretagne','N',3);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g038','Plougastel Athlé',0,'France - Bretagne','N',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g039','BCH FC',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g040','Blody-Hell Baketball',0,'France - Bretagne','N',2);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g041','Molard FC',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g042','Cercle Athlétisme Nord',0,'France - Bretagne','N',4);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g043','Football south island',0,'France - Bretagne','N',1);
+insert into Equipe (idE, nom, nombrepersonnes, nompays, hebergement, numSport) values ('g044','Equipe Foot AntoineL',0,'France - Bretagne','N',1);
 
 -- Les attributions sont fictives
 insert into Attribution values ('0350785N', 'g001', 11);
@@ -62,12 +67,6 @@ insert into Attribution values ('0350123A', 'g005', 8);
 insert into Attribution values ('0351234W', 'g001', 3);
 insert into Attribution values ('0351234W', 'g006', 10);
 insert into Attribution values ('0351234W', 'g007', 7);
-
-insert into Sport (idSport, libelleSport) values (1,'Football');
-insert into Sport (idSport, libelleSport) values (2,'Basketball');
-insert into Sport (idSport, libelleSport) values (3,'Handball');
-insert into Sport (idSport, libelleSport) values (4,'Athletisme');
-
 
 
 
