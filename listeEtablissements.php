68dc70889f9b248b1ad1dc0ef9fae51ad61e0d00
Hugo <huhu051@gmail.com>
2020-09-22 20:28:06 +0200

    Update listeEtablissements.php
    Amélioration de l'interface de la page listeEtablissements.php

--- listeEtablissements.php
+++ listeEtablissements.php
@@ -24,14 +24,16 @@ if (!selectBase($connexion))
 // CETTE PAGE CONTIENT UN TABLEAU CONSTITUÉ D'1 LIGNE D'EN-TÊTE ET D'1 LIGNE PAR
 // ÉTABLISSEMENT
 
-echo "
-<table width='70%' cellspacing='0' cellpadding='0' align='center' 
-class='tabNonQuadrille'>
-   <tr class='enTeteTabNonQuad'>
-      <td colspan='4'>Etablissements</td>
-   </tr>";
-     
-   $req=obtenirReqEtablissements();
+
+?>
+<div class="mx-auto" style="width: 900px;">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <td colspan='4'>Etablissements</td>
+            </tr>
+            <?php
+       $req=obtenirReqEtablissements();
    $rsEtab=mysqli_query( $connexion,$req);
    $lgEtab=mysqli_fetch_array($rsEtab);
    // BOUCLE SUR LES ÉTABLISSEMENTS
@@ -39,9 +41,8 @@ class='tabNonQuadrille'>
    {
       $idEtablissement=$lgEtab['idEtablissement'];
       $nom=$lgEtab['nom'];
-      echo $nom;
       echo"
-		<tr class='ligneTabNonQuad'>
+      <tr class='ligneTabNonQuad'>
          <td width='52%'>$nom</td>
          
          <td width='16%' align='center'> 
@@ -64,17 +65,81 @@ class='tabNonQuadrille'>
          else
          {
             echo "
-            <td width='16%'>&nbsp; </td>";          
+            <td width='16%'><center>Supprimer</center></td>";          
 			}
-			echo "
-      </tr>";
       $lgEtab=mysqli_fetch_array($rsEtab);
    }   
    echo "
-   <tr class='ligneTabNonQuad'>
-      <td colspan='4'><a href='creationEtablissement.php?action=demanderCreEtab'>
-      Création d'un établissement</a ></td>
-  </tr>
-</table>";
 
+";
 ?>
+</div>
+</table>
+<div class="mx-auto" style="width: 1200px;">
+<table class="table table-borderless">
+    <thead>
+        <tr class='ligneTabNonQuad'>
+            <td width='70%'></td>
+
+            <td colspan='4'><a href='creationEtablissement.php?action=demanderCreEtab'>
+                    Création d'un établissement</a></td>
+        </tr>
+</table>
+</div>
+
+<?php
+
+// echo "
+// <table width='70%' cellspacing='0' cellpadding='0' align='center' 
+// class='tabNonQuadrille'>
+//    <tr class='enTeteTabNonQuad'>
+//       <td colspan='4'>Etablissements</td>
+//    </tr>";
+     
+//    $req=obtenirReqEtablissements();
+//    $rsEtab=mysqli_query( $connexion,$req);
+//    $lgEtab=mysqli_fetch_array($rsEtab);
+//    // BOUCLE SUR LES ÉTABLISSEMENTS
+//    while ($lgEtab!=FALSE)
+//    {
+//       $idEtablissement=$lgEtab['idEtablissement'];
+//       $nom=$lgEtab['nom'];
+//       echo $nom;
+//       echo"
+// 		<tr class='ligneTabNonQuad'>
+//          <td width='52%'>$nom</td>
+         
+//          <td width='16%' align='center'> 
+//          <a href='detailEtablissement.php?idEtablissement=$idEtablissement'>
+//          Voir détail</a></td>
+         
+//          <td width='16%' align='center'> 
+//          <a href='modificationEtablissement.php?action=demanderModifEtab&amp;idEtablissement=$idEtablissement'>
+//          Modifier</a></td>";
+      	
+//          // S'il existe déjà des attributions pour l'établissement, il faudra
+//          // d'abord les supprimer avant de pouvoir supprimer l'établissement
+// 			if (!existeAttributionsEtab($connexion, $idEtablissement))
+// 			{
+//             echo "
+//             <td width='16%' align='center'> 
+//             <a href='suppressionEtablissement.php?action=demanderSupprEtab&amp;idEtablissement=$idEtablissement'>
+//             Supprimer</a></td>";
+//          }
+//          else
+//          {
+//             echo "
+//             <td width='16%'>&nbsp; </td>";          
+// 			}
+// 			echo "
+//       </tr>";
+//       $lgEtab=mysqli_fetch_array($rsEtab);
+//    }   
+//    echo "
+//    <tr class='ligneTabNonQuad'>
+//       <td colspan='4'><a href='creationEtablissement.php?action=demanderCreEtab'>
+//       Création d'un établissement</a ></td>
+//   </tr>
+// </table>";
+
+ ?>
