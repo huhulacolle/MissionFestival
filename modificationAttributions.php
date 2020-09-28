f8e2c8de02969ab52b2ad5b7739936062bb6f179
Romain <thiriot2002@gmail.com>
2020-09-28 13:55:34 +0200

    by commiting

--- modificationAttributions.php
+++ modificationAttributions.php
@@ -62,8 +62,8 @@ class='tabQuadrille'>";
       <td>&nbsp;</td>";
       
    $req=obtenirReqEtablissementsOffrantChambres();
-   $rsEtab=mysql_query($req, $connexion);
-   $lgEtab=mysql_fetch_array($rsEtab);
+   $rsEtab=mysqli_query($req, $connexion);
+   $lgEtab=mysqli_fetch_array($rsEtab);
 
    // Boucle sur les établissements (pour afficher le nom de l'établissement et 
    // le nombre de chambres encore disponibles)
@@ -79,7 +79,7 @@ class='tabQuadrille'>";
       echo "
       <td valign='top' width='$pourcCol%'><i>Disponibilités : $nbChLib </i> <br>
       $nom </td>";
-      $lgEtab=mysql_fetch_array($rsEtab);
+      $lgEtab=mysqli_fetch_array($rsEtab);
    }
    echo "
    </tr>"; 
@@ -88,8 +88,8 @@ class='tabQuadrille'>";
    // CHAMBRES ATTRIBUÉES ET LES LIENS POUR EFFECTUER OU MODIFIER LES ATTRIBUTIONS
          
    $req=obtenirReqIdNomGroupesAHeberger();
-   $rsGroupe=mysql_query($req, $connexion);
-   $lgGroupe=mysql_fetch_array($rsGroupe);
+   $rsGroupe=mysqli_query($req, $connexion);
+   $lgGroupe=mysqli_fetch_array($rsGroupe);
          
    // BOUCLE SUR LES GROUPES À HÉBERGER 
    while ($lgGroupe!=FALSE)
@@ -100,8 +100,8 @@ class='tabQuadrille'>";
       <tr class='ligneTabQuad'>
          <td width='25%'>$nom</td>";
       $req=obtenirReqEtablissementsOffrantChambres();
-      $rsEtab=mysql_query($req, $connexion);
-      $lgEtab=mysql_fetch_array($rsEtab);
+      $rsEtab=mysqli_query($req, $connexion);
+      $lgEtab=mysqli_fetch_array($rsEtab);
            
       // BOUCLE SUR LES ÉTABLISSEMENTS
       while ($lgEtab!=FALSE)
@@ -148,9 +148,9 @@ class='tabQuadrille'>";
                echo "<td class='reserveSiLien'>&nbsp;</td>";
             }
          }    
-         $lgEtab=mysql_fetch_array($rsEtab);
+         $lgEtab=mysqli_fetch_array($rsEtab);
       } // Fin de la boucle sur les établissements    
-      $lgGroupe=mysql_fetch_array($rsGroupe);  
+      $lgGroupe=mysqli_fetch_array($rsGroupe);  
    } // Fin de la boucle sur les groupes à héberger
 echo "
 </table>"; // Fin du tableau principal
